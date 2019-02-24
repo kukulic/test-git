c8908ae3b628882bc521af5d2827a7359cbd0500
kukulic <luka_kukulic@hotmail.com>
2019-02-25 00:16:23 +0100

    ponovno

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
     System.out.println("prvi commit");
-
-    System.out.println("nešto petjam što bi trebalo biti vidjivo kansije");
-
-    System.out.println("kreiranje prvog brancha");
-
-    System.out.println("prvi push na novi branch");
-
-    System.out.println("drugi push na novi branch");
+    
 
     }
 }
